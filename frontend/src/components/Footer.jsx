17bd6a378ba4d5a00a66864fcd7fda3b3095fa31
Ralph <Hernandezralphb@gmail.com>
2025-06-25 02:05:39 +0800

    first commit

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { assets } from "../assets/assets";
+
+const Footer = () => {
+    return (
+        <div>
+            <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm">
+                <div>
+                    <img src={assets.logo} className="mb-5 w-32" alt="" />
+                    <p className="w-full md:w-2/3 text-gray-600">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing
+                        elit. Iste, blanditiis! Lorem, ipsum dolor sit amet
+                        consectetur adipisicing elit. Accusantium error delectus
+                        esse. Maiores officia deleniti, laboriosam non autem
+                        nesciunt inventore!
+                    </p>
+                </div>
+                <div>
+                    <p className="text-xl font-medium mb-5">COMPANY</p>
+                    <ul className="flex flex-col gap-1 text-gray-600">
+                        <li>Home</li>
+                        <li>About</li>
+                        <li>Delivery</li>
+                        <li>Privacy Policy</li>
+                    </ul>
+                </div>
+
+                <div>
+                    <p className="text-xl font-medium mb-5">GET IN TOUCH</p>
+                    <ul className="flex flex-col gap-1 text-gray-600">
+                        <li>+1-212-345-6789</li>
+                        <li>Forever@contact.us</li>
+                    </ul>
+                </div>
+            </div>
+            <div>
+                <hr />
+                <p className="py-5 text-sm text-center">
+                    Copyright 2024 @ forever.com | All right reserved
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default Footer;
---
+++ frontend/src/components/OurPolicy.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { assets } from "../assets/assets";
+
+const OurPolicy = () => {
+    return (
+        <div className="flex flex-col sm:flex-row justify-around gap-12 sm:gap-2 text-center py-20 text-sm sm:text-sm md:text-base text-gray-700">
+            <div>
+                <img
+                    src={assets.exchange_icon}
+                    className="w-12 m-auto mb-5"
+                    alt=""
+                />
+                <p className="font-semibold">Easy Exchange Policy</p>
+                <p className="text-gray-400">
+                    We offer hassle free exhange policy
+                </p>
+            </div>
+            <div>
+                <img
+                    src={assets.quality_icon}
+                    className="w-12 m-auto mb-5"
+                    alt=""
+                />
+                <p className="font-semibold">7 Days Return Policy</p>
+                <p className="text-gray-400">
+                    We Provide 7 Days FreeRreturn Policy
+                </p>
+            </div>
+            <div>
+                <img
+                    src={assets.support_img}
+                    className="w-12 m-auto mb-5"
+                    alt=""
+                />
+                <p className="font-semibold">Best Customer Suppoert</p>
+                <p className="text-gray-400">
+                    We Provide 24/7 Customer Support
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default OurPolicy;
