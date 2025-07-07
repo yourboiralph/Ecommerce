9423da097f8d8915dc229241b3fb1c684a980025
Ralph <Hernandezralphb@gmail.com>
2025-07-07 18:19:49 +0800

    update

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,10 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { assets } from "../assets/assets";
 import { Link, NavLink } from "react-router-dom";
+import { ShopContext } from "../context/ShopContext";
 const Navbar = () => {
     const [visible, setVisible] = useState(false);
+    const { setShowSearch } = useContext(ShopContext);
     return (
         <div className="flex items-center justify-between py-5 font-medium">
             <Link to="/">
@@ -39,6 +41,7 @@ const Navbar = () => {
 
             <div className="flex items-center gap-6">
                 <img
+                    onClick={() => setShowSearch(true)}
                     src={assets.search_icon}
                     className="w-5 cursor-pointer"
                     alt=""
--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -5,11 +5,12 @@ import Title from "../components/Title";
 import ProductItem from "../components/ProductItem";
 
 const Collection = () => {
-    const { products } = useContext(ShopContext);
+    const { products, search, showSearch } = useContext(ShopContext);
     const [showFilter, setShowFilter] = useState(false);
     const [filterProducts, setFilterProducts] = useState([]);
     const [category, setCategory] = useState([]);
     const [subCategory, setSubCategory] = useState([]);
+    const [sortType, setSortType] = useState("relevant");
 
     const toggleCategory = (e) => {
         if (category.includes(e.target.value)) {
@@ -34,6 +35,12 @@ const Collection = () => {
     const applyFilter = () => {
         let productsCopy = products.slice();
 
+        if (showSearch && search) {
+            productsCopy = productsCopy.filter((item) =>
+                item.name.toLowerCase().includes(search.toLowerCase())
+            );
+        }
+
         if (category.length > 0) {
             productsCopy = productsCopy.filter((item) =>
                 category.includes(item.category)
@@ -49,13 +56,31 @@ const Collection = () => {
         setFilterProducts(productsCopy);
     };
 
-    useEffect(() => {
-        setFilterProducts(products);
-    }, []);
+    const sortProduct = () => {
+        let fpCopy = filterProducts.slice();
+
+        switch (sortType) {
+            case "low-high":
+                setFilterProducts(fpCopy.sort((a, b) => a.price - b.price));
+                break;
+
+            case "high-low":
+                setFilterProducts(fpCopy.sort((a, b) => b.price - a.price));
+                break;
+
+            default:
+                applyFilter();
+                break;
+        }
+    };
 
     useEffect(() => {
         applyFilter();
-    }, [category, subCategory]);
+    }, [category, subCategory, search, showSearch]);
+
+    useEffect(() => {
+        sortProduct();
+    }, [sortType]);
 
     return (
         <div className="flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t">
@@ -156,7 +181,10 @@ const Collection = () => {
                 <div className="flex justify-between text-base sm:text-2xl mb-4">
                     <Title text1={"ALL"} text2={"COLLECTIONS"} />
                     {/* Product Sort */}
-                    <select className="border-2 border-gray-300 text-sm px-2">
+                    <select
+                        onChange={(e) => setSortType(e.target.value)}
+                        className="border-2 border-gray-300 text-sm px-2"
+                    >
                         <option value="relevant">Sort by: Relevancy</option>
                         <option value="low-high">Sort by: Low to High</option>
                         <option value="high-low">Sort by: High to Low</option>
