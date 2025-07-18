7993f1ddd2196d2e0b3d2e1b1108609fcf46dcbc
ralph hernandez <mac@ralphs-MacBook-Air.local>
2025-07-18 08:46:26 +0800

    Updates with backend

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,8 +1,46 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+import Sidebar from './components/Sidebar'
+import Add from './pages/Add'
+import List from './pages/List'
+import Orders from './pages/Orders'
+
+import {Routes, Route} from 'react-router-dom'
+import { useState } from 'react'
+import Login from './components/Login'
+import { ToastContainer } from 'react-toastify';
+import { useEffect } from 'react'
+
+export const backendUrl = import.meta.env.VITE_BACKEND_URL
 
 const App = () => {
+
+  const [token, setToken] = useState(localStorage.getItem('token') ? localStorage.getItem('token') : '' )
+
+  useEffect(()=>{
+    localStorage.setItem('token', token)
+  }, [token])
+
   return (
-    <div>
+    <div className='bg-gray-50 min-h-screen'>
+      <ToastContainer />
+      {token === "" ? <Login setToken={setToken} /> : 
+      <>
+        <Navbar setToken={setToken} />
+        <hr />
+        <div className='flex w-full'>
+          <Sidebar />
+          <div className='w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base'>
+            <Routes>
+              <Route path='/add' element={<Add token={token}/>} />
+              <Route path='/list' element={<List token={token}/>} />
+              <Route path='/orders' element={<Orders token={token}/>} />
+            </Routes>
+          </div>
+        </div>
+      </>
+      }
+      
     </div>
   )
 }
---
+++ admin/src/pages/Add.jsx
@@ -0,0 +1,109 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+import { useState } from 'react'
+
+const Add = () => {
+
+  const [image1, setImage1] = useState(false)
+  const [image2, setImage2] = useState(false)
+  const [image3, setImage3] = useState(false)
+  const [image4, setImage4] = useState(false)
+
+  const [name, setName] = useState("")
+  const [description, setDescription] = useState("")
+  const [price, setPrice] = useState("")
+  const [category, setCategory] = useState("Men")
+  const [subCategory, setSubCategory] = useState("Topwear")
+  const [bestseller, setBestseller] = useState(false)
+  const [sizes, setSizes] = useState([])
+
+  return (
+    <form className='flex flex-col w-full items-start gap-3'>
+      <div>
+        <p className='mb-2'>Upload Image</p>
+        <div className='flex gap-2'>
+          <label htmlFor="image1">
+            <img className='w-20' src={assets.upload_area} alt="" />
+            <input onChange={(e) => setImage1(e.target.files[0])} type="file" id='image1' hidden/>
+          </label>
+          <label htmlFor="image2">
+            <img className='w-20' src={assets.upload_area} alt="" />
+            <input onChange={(e) => setImage2(e.target.files[0])} type="file" id='image2' hidden/>
+          </label>
+          <label htmlFor="image3">
+            <img className='w-20' src={assets.upload_area} alt="" />
+            <input onChange={(e) => setImage3(e.target.files[0])} type="file" id='image3' hidden/>
+          </label>
+          <label htmlFor="image4">
+            <img className='w-20' src={assets.upload_area} alt="" />
+            <input onChange={(e) => setImage4(e.target.files[0])} type="file" id='image4' hidden/>
+          </label>
+        </div>
+      </div>
+
+      <div className='w-full'>
+        <p className='mb-2'>Product Name</p>
+        <input className='w-full max-w-[500px] px-3 py-2' type="text" placeholder='Type here' required />
+      </div>
+      <div className='w-full'>
+        <p className='mb-2'>Product Description</p>
+        <textarea className='w-full max-w-[500px] px-3 py-2' type="text" placeholder='Write content here' required />
+      </div>
+      
+      <div className='flex flex-col sm:flex-row gap-2 w-full sm:gap-8'>
+        <div>
+          <p className='mb-2'>Product Category</p>
+          <select className='w-full px-3 py-2'>
+            <option value="Men">Men</option>
+            <option value="Women">Women</option>
+            <option value="Kids">Kids</option>
+          </select>
+        </div>
+
+        <div>
+          <p className='mb-2'>Sub Category</p>
+          <select className='w-full px-3 py-2'>
+            <option value="Topwear">Topwear</option>
+            <option value="Bottomwear">Bottomwear</option>
+            <option value="Winterwear">Winterwear</option>
+          </select>
+        </div>
+
+        <div>
+          <p className='mb-2'>Product Price</p>
+          <input className='w-full px-3 py-2 sm:w-[120px]' type="Number" placeholder='25$' />
+        </div>
+      </div>
+
+      <div>
+        <p className='mb-2'>Product Sizes</p>
+        <div className='flex gap-3'>
+          <div>
+            <p className='bg-slate-200 px-3 py-1 cursor-pointer'>S</p>
+          </div>
+          <div>
+            <p className='bg-slate-200 px-3 py-1 cursor-pointer'>M</p>
+          </div>
+          <div>
+            <p className='bg-slate-200 px-3 py-1 cursor-pointer'>L</p>
+          </div>
+          <div>
+            <p className='bg-slate-200 px-3 py-1 cursor-pointer'>XL</p>
+          </div>
+          <div>
+            <p className='bg-slate-200 px-3 py-1 cursor-pointer'>XXL</p>
+          </div>
+        </div>
+      </div>
+
+      <div className='flex gap-2 mt-2'>
+        <input type="checkbox" id='bestseller' />
+        <label className='cursor-pointer' htmlFor="bestseller">Add to bestseller</label>
+      </div>
+
+      <button type='submit' className='w-28 py-3 mt-4 bg-black text-white'>ADD</button>
+    </form>
+  )
+}
+
+export default Add
